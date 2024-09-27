b28a0fd152c28f161af15252e97fe06a66b68bfd
Gabriel Kozuch <kozuch8@uniba.sk>
2024-09-27 21:36:03 +0200

    added third tower

--- hanoi.c
+++ hanoi.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 
-void hanoi(int n, int a, int b)
+void hanoi(int n, int a, int b, int c)
 {
   if (n == 1) 
-    printf("move stone 1 from %d to %d\n", a, b);
+    printf("move stone 1 from %d to %d\n", a, b, c);
   else
   {
-    hanoi(n - 1, a, 6 - a);
-    printf("move stone %d from %d to %d\n", n, a, b);
-    hanoi(n, 6 - a - b, b);
+    hanoi(n - 1, a, 6 - a, c);
+    printf("move stone %d from %d to %d\n", n, a, b, c);
+    hanoi(n, 6 - a - b, b, c);
   }
 }
 
@@ -16,6 +16,6 @@ int main()
 {
   int n = 4;
 
-  hanoi(n, 1, 2);
+  hanoi(n, 1, 2, 3);
   return 0;
 }
